e4a8a0a046ca976c4acda03d93c1741f187784e0
olesgergun <gerguno@gmail.com>
2021-02-02 20:27:38 +0200

    mobile start+

--- components/FullsizeVideo.js
+++ components/FullsizeVideo.js
@@ -1,7 +1,7 @@
 export function FullsizeVideo({src, ext}) {
     return (
         <div className="fullsize">
-            <video src={src} type={ext} loop autoPlay muted/>
+            <video src={src} type={ext} loop autoPlay muted playsInline/>
         </div>
     )
 }
